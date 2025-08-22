02e5264bba85efa01e33a4efcec5a01849331c45
Ravi Srinivasan <work.ravis@gmail.com>
2025-08-22 13:39:41 +0530

    Create PolygonAreaCalculator.py

---
+++ PolygonAreaCalculator.py
@@ -0,0 +1,54 @@
+class Rectangle:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+
+  def set_width(self, width):
+    self.width = width
+  
+  def set_height(self, height):
+    self.height = height
+
+  def get_area(self):
+    return self.height * self.width
+
+  def get_perimeter(self):
+    return 2 * (self.height + self.width)
+
+  def get_diagonal(self):
+    return (self.height**2 + self.width**2) ** 0.5
+
+  def get_picture(self):
+    if self.width > 50 or self.height > 50:
+      return "Too big for picture."
+    rectangle = ("*" * self.width + "\n") * self.height
+    return rectangle
+
+  def get_amount_inside(self, ashape):
+    max_width = self.width // ashape.width
+    max_height = self.height // ashape.height
+    return max_width * max_height
+
+  def __str__(self):
+    return f'Rectangle(width={self.width}, height={self.height})'
+
+
+class Square(Rectangle):
+  def __init__(self, side):
+    self.width = side
+    self.height = side
+  
+  def set_side(self, side):
+    self.width = side
+    self.height = side
+
+  def __str__(self):
+    return f'Square(side={self.width})'
+
+  def set_width(self, width):
+    self.width = width
+    self.height = width
+  
+  def set_height(self, height):
+    self.height = height
+    self.width = height
